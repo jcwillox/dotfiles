b7dd463aa1e0e75644429762e8e24be507a3e784
Josh Willox <joshwillox@gmail.com>
2022-01-23 00:55:51 +1100

    Add idea and pycharm aliases

--- zsh/wsl.zsh
+++ zsh/wsl.zsh
@@ -1,3 +1,13 @@
 function o {
     explorer.exe "${1:-.}"
 }
+
+function idea {
+    powershell.exe -noprofile -ex unrestricted -c \
+        "& \$env:LOCALAPPDATA/JetBrains/Toolbox/apps/IDEA-U/ch-0/*/bin/idea64.exe ${1:-.} ${@:2}"
+}
+
+function pycharm {
+    powershell.exe -noprofile -ex unrestricted -c \
+        "& \$env:LOCALAPPDATA/JetBrains/Toolbox/apps/PyCharm-P/ch-0/*/bin/pycharm64.exe ${1:-.} ${@:2}"
+}

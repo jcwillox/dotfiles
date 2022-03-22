d2a0c99617592d0b6a69f0b5371189b15de91b62
Josh Willox <joshwillox@gmail.com>
2022-03-22 17:47:05 +1100

    Support forwarding ssh-agent from Windows

--- zsh/wsl.zsh
+++ zsh/wsl.zsh
@@ -11,3 +11,12 @@ function pycharm {
     powershell.exe -noprofile -ex unrestricted -c \
         "& \$env:LOCALAPPDATA/JetBrains/Toolbox/apps/PyCharm-P/ch-0/*/bin/pycharm64.exe ${1:-.} ${@:2}"
 }
+
+# forward ssh-agent from windows
+export SSH_AUTH_SOCK=$HOME/.ssh/agent.sock
+if ! ps -auxww | grep -q "[n]piperelay.exe -ei -s //./pipe/openssh-ssh-agent"; then
+  if [[ -S $SSH_AUTH_SOCK ]]; then
+      rm $SSH_AUTH_SOCK
+  fi
+  (setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork &) >/dev/null 2>&1
+fi

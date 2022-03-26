6e9a34e663e0eb3eb54ec3725708164c6907a3f9
Josh Willox <joshwillox@gmail.com>
2022-03-26 21:19:22 +1100

    Support wsl instances without windows paths

--- zsh/wsl.zsh
+++ zsh/wsl.zsh
@@ -1,5 +1,5 @@
 function o {
-    explorer.exe "${1:-.}"
+    /mnt/c/Windows/explorer.exe "${1:-.}"
 }
 
 function idea {
@@ -20,3 +20,13 @@ if ! ps -auxww | grep -q "[n]piperelay.exe -ei -s //./pipe/openssh-ssh-agent"; t
   fi
   (setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork &) >/dev/null 2>&1
 fi
+
+# facilitate windows executable interop
+function exe {
+    target=$(wslpath "$(awk -F$'\r' 'NR == 1 {printf "%s", $1}' <<< $(/mnt/c/Windows/System32/cmd.exe /c where $1 2>/dev/null))")
+    if [[ "$target" == "." ]]; then
+      echo "Command '$1' not found."
+    else
+        $target ${@:2}
+    fi
+}
